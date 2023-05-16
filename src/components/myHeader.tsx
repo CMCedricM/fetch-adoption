5fea99dd260f60d2d7a88a200630323fd7108b49
Cedric Men <cedricm13@outlook.com>
2023-05-15 23:09:46 -0700

    started homepage design

---
+++ src/components/myHeader.tsx
@@ -0,0 +1,15 @@
+const MyHeader = () => {
+  return (
+    <div className="w-[99vw] h-full flex flex-auto flex-row items-center justify-center px-2 py-2">
+      <div className="grow">
+        <h1>Fetch</h1>
+      </div>
+
+      <div>
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MyHeader;
