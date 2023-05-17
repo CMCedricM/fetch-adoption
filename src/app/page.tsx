42fc687b9e4cd6e69ac7674d95661ba441157a9b
Cedric Men <cedricm13@outlook.com>
2023-05-16 20:21:28 -0700

    another fix to readbility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ export default function Home() {
   return (
     <main className="md:h-[88vh] flex flex-col items-center">
       <div className="flex justify-center w-full h-full px-3 lg:pb-0 pb-3">
-        <div className="z-2 mt-[9vh] ml-[2vh] md:mt-[11vh] md:ml-[11vh] px-3 flex flex-col items-start justify-center absolute  w-[50%] text-[#40e024]">
-          <h1 className="text-[64px] lg:text-[72px] font-bold w-[50%] lg:w-full">
+        <div className="z-2 mt-[8vh] ml-[2vh] md:mt-[11vh] md:ml-[11vh] px-3 flex flex-col items-start justify-center absolute  w-[50%] text-[#00ff15] md:text-[#40e024]">
+          <h1 className="text-[60px] lg:text-[72px] font-bold w-[50%] lg:w-full drop-shadow-lg">
             Find Your
           </h1>
-          <h1 className="text-[64px] lg:text-[72px] lg:ml-[12vh] font-bold font-Rubik w-[50%] lg:w-full text-[#36965]">
+          <h1 className="text-[60px] lg:text-[72px] lg:ml-[12vh] font-bold font-Rubik w-[50%] lg:w-full text-[#36965] drop-shadow-lg">
             Best Friend
           </h1>
           <div className="items-center w-full text-center mt-5 ">
             <div>
               <button
-                className=" w-[80%] lg:w-[25%] backdrop-blur-md  bg-[#40e024]/50 pt-4 pb-3 
-              rounded-xl text-black text-lg font-bold font-Rubik  hover:bg-[#40e024]/40"
+                className=" w-[80%] lg:w-[25%] backdrop-blur-md  bg-[#40e024]/80 pt-4 pb-3 px-2
+              rounded-xl text-black text-lg font-bold font-Rubik  hover:bg-[#40e024]/60 drop-shadow-lg"
               >
                 {`Let's Play Fetch!`}
               </button>
