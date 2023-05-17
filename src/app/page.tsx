a64ccf7da7cb7738a3d7ac488a62fe0b6ddfeb19
Cedric Men <cedricm13@outlook.com>
2023-05-16 17:49:37 -0700

    removed weird top white border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import smallDogImage from "../srcImages/dog-6289929_960_720.jpg";
 
 export default function Home() {
   return (
-    <main className="md:h-[88vh] flex flex-col items-center">
+    <main className="md:h-[88vh] flex flex-col items-center mt-2">
       <div className="flex justify-center w-full h-full px-3 lg:pb-0 pb-3">
         <div className="mt-[10vh] px-3 flex flex-col items-start justify-center absolute  w-[50%] text-[#038900]">
           <h1 className="text-[64px] lg:text-[72px] font-bold w-[50%] lg:w-full">
@@ -17,7 +17,7 @@ export default function Home() {
           <div className="items-center w-full text-center mt-5  ">
             <div>
               <button
-                className=" w-[50%] lg:w-[25%] backdrop-blur-md  bg-[#36965C]/30 p-4 
+                className=" w-[80%] lg:w-[25%] backdrop-blur-md  bg-[#36965C]/30 p-4 
               rounded-xl text-[#7AE868] text-lg font-bold font-Rubik hover:text-[#174b0f] hover:bg-[#038900]/30"
               >
                 {`Let's Play Fetch!`}
--- src/components/myHeader.tsx
+++ src/components/myHeader.tsx
@@ -1,6 +1,6 @@
 const MyHeader = () => {
   return (
-    <div className="w-full flex flex-auto flex-row items-center justify-center px-10 py-2 border border-b-black">
+    <div className="w-full flex flex-auto flex-row items-center justify-center px-10 py-2 border-b border-b-black">
       <div className="grow text-[48px] text-[#2f922e] font-Rubik font-semibold">
         <h1>Fetch</h1>
       </div>
