1710671ae87484d6bdc5b6a6b671f366b55834e4
Cedric Men <cedricm13@outlook.com>
2023-05-18 16:52:05 -0700

    fixed homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,20 @@ export default function Home() {
           className="z-2 h-[50%] mt-[12vh] ml-[4vw] md:mt-[15vh] md:ml-[11vh] px-3 flex flex-col gap-2 items-start md:items-center
         justify-center absolute w-[50%] text-[#00ff15] md:text-[#40e024] text-center"
         >
-          <h1 className="text-[60px] lg:text-[60px] font-bold w-[50%] drop-shadow-lg md:bg-black/40 rounded-md">
+          <h1 className="text-[60px] lg:text-[60px] font-bold w-[50%] drop-shadow-lg md:bg-[#000000]/40 rounded-md">
             Find Your
           </h1>
           <h1
             className="text-[60px] lg:text-[60px] font-bold font-Rubik 
-          w-[50%] text-[#36965] drop-shadow-lg md:bg-black/40 rounded-md"
+          w-[50%] text-[#36965] drop-shadow-lg md:bg-[#000000]/40 rounded-md"
           >
             Best Friend
           </h1>
 
           <Link
             href="/home"
-            className=" w-[80%] md:w-[30%] backdrop-blur-md  bg-[#40e024]/80 pt-4 pb-3 px-2
-              rounded-xl text-black text-lg font-bold font-Rubik  hover:bg-[#40e024]/60 drop-shadow-lg ml-[3vw] md:ml-0 "
+            className=" w-[80%] md:w-[30%] backdrop-blur-md   bg-[#40e024]/60 pt-4 pb-3 px-2
+              rounded-xl text-[#000000] text-lg font-bold font-Rubik  hover:bg-[#40e024]/80 drop-shadow-lg ml-[3vw] md:ml-0 "
           >
             <button>{`Let's Play Fetch!`} </button>
           </Link>
