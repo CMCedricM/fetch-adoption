2e95a4fa4429f5c396a23bdeb9db1807ba3ef6e5
Cedric Men <cedricm13@outlook.com>
2023-05-29 16:01:59 -0700

    added matche me details

--- src/components/dashboard/MatchMe.tsx
+++ src/components/dashboard/MatchMe.tsx
@@ -40,11 +40,25 @@ const MatchMe = ({ selectedDogs }: MatchMeProps) => {
     }
   }, [getTheMatch]);
 
+  const [showQuestion, setShowQuestion] = useState(false);
   return (
     <div
       className="flex flex-col items-center gap-2 font-Rubik"
       data-test="match_button"
     >
+      <FetchModal
+        openState={[showQuestion, setShowQuestion]}
+        colorScheme="bg-[#E0E1BC]"
+        dialogTitle={"How Does This Work?"}
+      >
+        <div className=" py-2 mt-4 w-full flex flex-col items-center justify-center font-Rubik border-2 rounded-md">
+          <ul className="flex flex-col list-decimal font-semibold w-[10vw] items-start">
+            <li>Select the Star Button</li>
+            <li>{`Select the 'Match Me' Button`}</li>
+            <li>{`Now you can see your match!`}</li>
+          </ul>
+        </div>
+      </FetchModal>
       <FetchModal
         openState={[showMatchModal, setShowMatchModal]}
         colorScheme="bg-[#E0E1BC]"
@@ -70,17 +84,27 @@ const MatchMe = ({ selectedDogs }: MatchMeProps) => {
         </div>
       </FetchModal>
       <div className="flex flex-row md:grid md:grid-rows-2 items-center gap-2 p-4">
-        <button
-          title={`${!allowMatch ? "Select Some Dogs To Find Your Match" : ""}`}
-          className={` rounded-md ${
-            allowMatch
-              ? "bg-button_green cursor-pointer"
-              : "bg-gray cursor-not-allowed "
-          } p-2 `}
-          onClick={() => (allowMatch ? setGetTheMatch(true) : "")}
-        >
-          Match Me
-        </button>
+        <div className="flex flex-row gap-2">
+          <button
+            onClick={() => setShowQuestion(true)}
+            className="bg-button_green cursor-pointer px-3 rounded-full"
+          >
+            ?
+          </button>
+          <button
+            title={`${
+              !allowMatch ? "Select Some Dogs To Find Your Match" : ""
+            }`}
+            className={` rounded-md ${
+              allowMatch
+                ? "bg-button_green cursor-pointer"
+                : "bg-gray cursor-not-allowed "
+            } p-2 `}
+            onClick={() => (allowMatch ? setGetTheMatch(true) : "")}
+          >
+            Match Me
+          </button>
+        </div>
         <button
           className={` rounded-md ${
             allowMatch ? "bg-button_green cursor-pointer" : "hidden"
