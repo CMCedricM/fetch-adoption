ff8e847d5785f1f34c4333de13507ea2ecab7c32
Cedric Men <cedricm13@outlook.com>
2023-05-29 21:04:26 -0700

    added ability to disable breed

--- src/components/combobox/fetchComboBox.tsx
+++ src/components/combobox/fetchComboBox.tsx
@@ -6,28 +6,35 @@ type FetchBoxProps = {
   selectedItem: [string[], Dispatch<SetStateAction<string[]>>];
   itemsList: string[];
   className?: string;
+  isDisabled?: boolean;
 };
 
 const FetchComboBox = ({
   selectedItem,
   itemsList,
   className,
+  isDisabled,
 }: FetchBoxProps) => {
   const [selectItem, setSelectedItem] = selectedItem;
   const [query, setQuery] = useState<string>("");
 
   return (
     <div>
-      <Combobox value={selectItem} onChange={setSelectedItem}>
+      <Combobox
+        value={selectItem}
+        onChange={setSelectedItem}
+        disabled={isDisabled ? true : false}
+      >
         <div className="relative w-full cursor-default overflow-hidden rounded-md bg-white text-left shadow-md ">
           <Combobox.Input
-            className="w-full border-none py-2 pl-3 text-sm overflow-hidden z-0"
+            className="w-full border-none py-2 pl-3 text-sm overflow-hidden z-0 disabled:bg-gray/60"
             onChange={(event) => setQuery(event.target.value)}
           />
-          <Combobox.Button className=" bg-niceWhite absolute inset-y-0 right-0 flex items-center pr-2">
+          <Combobox.Button className=" bg-niceWhite disabled:bg-gray/20 absolute inset-y-0 right-0 flex items-center px-2">
             <ChevronDownIcon
-              className="h-5 w-5 text-gray-400"
+              className="h-5 w-5"
               aria-hidden="true"
+              color={`${isDisabled ? "gray" : ""}`}
             />
           </Combobox.Button>
         </div>
